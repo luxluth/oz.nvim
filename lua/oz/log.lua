bd47afccde5f3ad1161c4c19baccc232b39caa53
luxluth <delphin.blehoussi93@gmail.com>
2024-03-29 10:56:54 +0100

    Logging reopenning The previous `OzOpenLog` cannot be reused after one invoke. The method close been unimplemented, Now on open we listen to the `on_detach` event on the logger buffer

--- lua/oz/log.lua
+++ lua/oz/log.lua
@@ -55,32 +55,38 @@ end
 function LogBuf:open_log()
   if self.buf.open ~= true then
     self.buf.nr = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_buf_attach(self.buf.nr, false, {
+      on_detach = function()
+        self.buf.nr = nil
+        self.buf.open = false
+      end,
+    })
     vim.api.nvim_buf_set_name(self.buf.nr, "[oz.nvim::Logger]")
     self.buf.open = true
     self:sync_lines()
   end
 end
 
-function LogBuf:close_log()
-  if self.buf.open ~= false then
-    if self.buf.nr ~= nil then
-      local win_id = vim.fn.bufwinid(self.buf.nr)
-      if win_id >= 0 then
-        vim.api.nvim_win_close(win_id, true)
-      end
-      vim.api.nvim_buf_delete(self.buf.nr, { force = true })
-    end
-    self.buf.nr = nil
-    self.buf.open = false
-  end
-end
-
-function LogBuf:toogle_log()
-  if self.buf.open then
-    self:close_log()
-  else
-    self:open_log()
-  end
-end
+-- function LogBuf:close_log()
+--   if self.buf.open ~= false then
+--     if self.buf.nr ~= nil then
+--       local win_id = vim.fn.bufwinid(self.buf.nr)
+--       if win_id >= 0 then
+--         vim.api.nvim_win_close(win_id, true)
+--       end
+--       vim.api.nvim_buf_delete(self.buf.nr, { force = true })
+--     end
+--     self.buf.nr = nil
+--     self.buf.open = false
+--   end
+-- end
+--
+-- function LogBuf:toogle_log()
+--   if self.buf.open then
+--     self:close_log()
+--   else
+--     self:open_log()
+--   end
+-- end
 
 return LogBuf
